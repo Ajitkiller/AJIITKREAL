1c15b5a782582f43665f5c1edfffc36f560f8ed6
Debraj <58354292+Ajitkiller@users.noreply.github.com>
2020-03-02 11:21:34 +0530

    first manually commit
    no such desc

---
+++ GeneralFunc.java
@@ -0,0 +1,21 @@
+package GeneralMethod;
+
+public class GeneralFunc {
+
+	public static void main(String[] args) {
+		
+		public void fireFoxProIni()
+		{
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
